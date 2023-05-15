f92e94c95bb784677b774680dcb363fa47c4e005
Mehmet Yigit Erdem <meerdem2004@gmail.com>
2023-05-15 22:53:27 +0300

    dizide Binary search ile arama

---
+++ binary search/binary search/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static int BinarySearch(int[] dizi, int aranan, int baslangic, int bitis)
+    {
+        if (baslangic <= bitis)
+        {
+            int orta = (baslangic + bitis) / 2;
+
+            if (dizi[orta] == aranan)
+                return orta;
+
+            if (dizi[orta] < aranan)
+                return BinarySearch(dizi, aranan, orta + 1, bitis);
+
+            if (dizi[orta] > aranan)
+                return BinarySearch(dizi, aranan, baslangic, orta - 1);
+        }
+
+        return -1; 
+    }
+
+    static void Main()
+    {
+        int[] dizi = { 4, 8, 3, 84, 47, 76, 9, 24, 68 };
+
+        Array.Sort(dizi); 
+
+        Console.Write("Aranacak elemanı girin: ");
+        int aranan = Convert.ToInt32(Console.ReadLine());
+
+        int sonuc = BinarySearch(dizi, aranan, 0, dizi.Length - 1);
+
+        if (sonuc != -1)
+            Console.WriteLine("Aranan eleman dizide bulundu!");
+        else
+            Console.WriteLine("Aranan eleman dizide bulunamadı!");
+
+        Console.ReadLine();
+    }
+}
